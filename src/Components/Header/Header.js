8d4b74ab0968ae51994aff87dfaf250f00b93fb1
QNabila <41321919+QNabila@users.noreply.github.com>
2021-09-26 23:00:46 +0600

    header added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <h2>Women In Tech!</h2>
+            <h4>Make The Best Female Engineer's Team For Your Company</h4>
+            <p>Total Budget: 10 Millions</p>
         </div>
     );
 };
